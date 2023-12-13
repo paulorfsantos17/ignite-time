9dea779beb75b526466545fe65a359204b839ba7
Paulo <paulorfsantos17@gmail.com>
2023-12-13 19:32:52 -0300

    feat: add  form validation and ensure countdown update numbers

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components/Header'
+import { Header } from '../../components/Header'
+import { LayoutContainer } from './style'
 
 export default function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </LayoutContainer>
   )
 }
